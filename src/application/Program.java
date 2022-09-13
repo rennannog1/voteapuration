64eb191fae3f6a5965e98513bd42abb0172e2e39
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-13 09:49:24 -0300

    Project done

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,44 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter file full path: ");
+			String path = sc.nextLine();
+			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+				Map <String, Integer>map = new HashMap<>();
+				String line = br.readLine();
+				while (line != null) {
+					String [] fields = line.split(",");
+					int votes = Integer.parseInt(fields[1]);
+					if(map.containsKey(fields[0])==true) {
+							votes = votes + map.get(fields[0]);
+					}
+					
+					map.put(fields[0], votes);
+					line = br.readLine();
+					}
+					Set <String> set = map.keySet();
+					
+					for (String a:set) {
+						System.out.println(a + ": " + map.get(a));
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+	
 
 }
+
+
--- src/entities/Candidate.java
+++ src/entities/Candidate.java
@@ -1,5 +1,38 @@
 package entities;
 
+import java.util.Objects;
+
 public class Candidate {
+	String name;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	public Candidate(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Candidate other = (Candidate) obj;
+		return Objects.equals(name, other.name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
